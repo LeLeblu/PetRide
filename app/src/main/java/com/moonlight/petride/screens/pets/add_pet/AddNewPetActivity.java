8096793748ad371a8da04e636e86a71bb7de8610
Leleblu <ladyrg123@gmail.com>
2026-04-07 12:50:49 -0500

    feat: Creacion de activities para el proyecto

---
+++ app/src/main/java/com/moonlight/petride/screens/pets/add_pet/AddNewPetActivity.java
@@ -0,0 +1,26 @@
+package com.moonlight.petride.screens.pets.add_pet;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.moonlight.petride.R;
+
+public class AddNewPetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_new_pet);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
